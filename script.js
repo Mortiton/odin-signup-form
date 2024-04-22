f8ef07131715963d010828d2d7b0ec38ab3f32e0
Zoe <159435044+Mortiton@users.noreply.github.com>
2024-04-22 06:33:40 +0100

    Modify background, add form and validations

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('#submit-btn');
+const passField = document.querySelector('#password')
+const passConfirm = document.querySelector('#confirm-password')
+const errorMsg = document.querySelector('#psw-error')
+const form = document.querySelector('form')
+
+function submitClicked() {
+    matchPasswords()
+}
+
+
+function matchPasswords() {
+    if (passField.value !== passConfirm.value) {
+        errorMsg.textContent="Passwords must match!"
+        passConfirm.style.border=("1px solid red")
+        passField.style.border=("1px solid red")
+    }
+    else {
+        form.submit();
+        console.log("submitted")
+        form.resetFields();
+    }
+}
+
+btn.addEventListener('click', submitClicked);
